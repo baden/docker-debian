a11f56ab9ad9fe26fca9a4734e464c7d155754e5
Denys Batrak <baden.i.ua@gmail.com>
2016-08-09 03:11:11 +0300

    Enable multiple run entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,14 @@ if [ $USER_ID ]; then
     #USER_ID=${USER_ID:-0}
     #GROUP_ID=${GROUP_ID:-0}
 
-    groupadd -g $GROUP_ID composer
-    useradd -d /home/composer -u $USER_ID -g $GROUP_ID composer
-    mkdir -p /home/composer
-    chown $USER_ID:$GROUP_ID /home/composer
+    # if ! getent group composer >/dev/null; then
+    if [ -z "$(getent group composer)" ]; then
+      groupadd -g $GROUP_ID composer
+      useradd -d /home/composer -u $USER_ID -g $GROUP_ID composer
+      mkdir -p /home/composer
+      chown $USER_ID:$GROUP_ID /home/composer
+    fi
+
 
     exec runuser -u composer -- "$@"
 
